a3cb280ae10e14a59724645932e824a8b2900a14
Bryann Alfaro <alf19372@uvg.edu.gt>
2022-02-25 18:51:46 -0600

    feat: Carga de base de datos

--- main.py
+++ main.py
@@ -1,5 +1,17 @@
 import pandas as pd
 
-df = pd.read_spss('XgKXvctBSUEXad69WYnJq6UDiVYBim0k.sav')
+#Carga de datos
+df1 = pd.read_spss('2009.sav')
+df2 = pd.read_spss('2010.sav')
+df3 = pd.read_spss('2011.sav')
+df4 = pd.read_spss('2012.sav')
+df5 = pd.read_spss('2013.sav')
+df6 = pd.read_spss('2014.sav')
+df7 = pd.read_spss('2015.sav')
+df8 = pd.read_spss('2016.sav')
+df9 = pd.read_spss('2017.sav')
+df10 = pd.read_spss('2018.sav')
 
-print(df.head())
+
+df = df1.append([df2,df3,df4,df5,df6,df7,df8,df9,df10],ignore_index=True)
+print(df.shape())
