aa43271795ba4d837d5d6fae6a93e6de02ca7f67
Raúl Angel Jiménez <raulangelj@gmail.com>
2022-03-18 22:23:01 -0600

    remove duplicate columns

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
+# %%
 from matplotlib.pyplot import axis
 import pandas as pd
 
-#Carga de datos
-'''df1 = pd.read_spss('2009.sav')
+# %%
+# Carga de datos
+df1 = pd.read_spss('2009.sav')
 df2 = pd.read_spss('2010.sav')
 df3 = pd.read_spss('2011.sav')
 df4 = pd.read_spss('2012.sav')
@@ -13,6 +15,7 @@ df8 = pd.read_spss('2016.sav')
 df9 = pd.read_spss('2017.sav')
 df10 = pd.read_spss('2018.sav')
 
+# %%
 df1.columns = df1.columns.str.lower()
 df2.columns = df2.columns.str.lower()
 df3.columns = df3.columns.str.lower()
@@ -24,19 +27,34 @@ df8.columns = df8.columns.str.lower()
 df9.columns = df9.columns.str.lower()
 df10.columns = df10.columns.str.lower()
 
-#df1.columns.str.lower()
+# %%
+# ARREGLO DE DATOS
+# REMPLAZO DE LA COLUMNA munpnap POR mupnap DEBIDO A QUE ES LA MISMA DESCRIPCION
+df6['mupnap'] = df6.pop('munpnap')
+df7['mupnap'] = df7.pop('munpnap')
+df8['mupnap'] = df8.pop('munpnap')
+df9['mupnap'] = df9.pop('munpnap')
+df10['mupnap'] = df10.pop('munpnap')
+
+
+# %%
+# REMPLAZO DE LA COLUMNA grupetma POR gretnm DEBIDO A QUE ES LA MISMA DESCRIPCION
+df2['gretnm'] = df2.pop('grupetma')
+df3['gretnm'] = df3.pop('grupetma')
+df4['gretnm'] = df4.pop('grupetma')
+
+# %%
+# REMPLAZO DE LA COLUMNA munnam POR mupnam DEBIDO A QUE ES LA MISMA DESCRIPCION
+df4['mupnam'] = df4.pop('munnam')
+
+# df1.columns.str.lower()
 #df = df1.append([df2.columns.str.lower(),df3.columns.str.lower(),df4.columns.str.lower(),df5.columns.str.lower(),df6.columns.str.lower(),df7.columns.str.lower(),df8.columns.str.lower(),df9.columns.str.lower(),df10.columns.str.lower()],ignore_index=True)
 
-df = pd.concat([df1,df2,df3,df4,df5,df6,df7,df8,df9,df10],axis=0,ignore_index=True)
-#df.to_csv('datosBase.csv')
-df['mupnap'] = df['mupnap']+df['munpnap']
-df['gretnm'] = df['gretnm']+df['grupetma']
-df['mupnam'] = df['mupnam']+df['munnam']
+# %%
+df = pd.concat([df1, df2, df3, df4, df5, df6, df7, df8,
+               df9, df10], axis=0, ignore_index=True)
+df.to_csv('datosBase.csv')
 
+# %%
 print(df.shape)
-print(df.columns.to_list())'''
-
-d = {'col1': ['Pedro', 'Juan'], 'col2': ['Pedro1', 'Juan1']}
-df = pd.DataFrame(data=d)
-df['col3'] = df['col1']+df['col2']
-print(df)
+print(df.columns.to_list())
